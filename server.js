70f7de5159ddfef5025b8d7673da1dd8fe3e4a48
Ziqi Wang <ziqiwang@Ziqis-MacBook-Air.local>
2023-05-19 23:39:59 -0500

    Add route for buyer bags

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ const ownerAccountSettingsRouter = require("./routes/owners/owners")
 const ownerBagsRouter = require("./routes/owners/ownerBags")
 const ownerOrdersRouter = require("./routes/owners/ownerOrders")
 const buyerAccountSettingsRouter = require("./routes/buyers/buyers")
+const buyerBagsRouter = require("./routes/buyers/buyerBags")
 
 app.use("/owners", ownerAccountSettingsRouter)
 app.use("/ownerBags", ownerBagsRouter)
 app.use("/ownerOrders", ownerOrdersRouter)
 
 app.use("/buyers", buyerAccountSettingsRouter)
+app.use("/buyerBags", buyerBagsRouter)
 
 app.listen(3000)
