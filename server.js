386b6ebab85135dd194be1de0b5e9e2fa62e88bd
Ziqi Wang <ziqiwang@Ziqis-MacBook-Air.local>
2023-05-19 23:24:08 -0500

    Add route for owner account settings

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require("express")
 const app = express()
 
 app.use(express.static("public"))
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.set("view engine", "ejs")
 
-// const userRouter = require("./routes/users")
+const ownerAccountSettingsRouter = require("./routes/owners/owners")
 
-// app.use("/users", userRouter)
+app.use("/owners", ownerAccountSettingsRouter)
 
 app.get('/', (req, res) => {
     res.render('index')
 })
+
 
 app.listen(3000)
