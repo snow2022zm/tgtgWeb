f43cd3a0078db3e98576bdd6ceb8619029f5731b
Ziqi Wang <ziqiwang@Ziqis-MacBook-Air.local>
2023-05-19 23:37:23 -0500

    Add route for owner orders

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.set("view engine", "ejs")
 
 const ownerAccountSettingsRouter = require("./routes/owners/owners")
 const ownerBagsRouter = require("./routes/owners/ownerBags")
+const ownerOrdersRouter = require("./routes/owners/ownerOrders")
 const buyerAccountSettingsRouter = require("./routes/buyers/buyers")
 
 app.use("/owners", ownerAccountSettingsRouter)
 app.use("/ownerBags", ownerBagsRouter)
+app.use("/ownerOrders", ownerOrdersRouter)
 
 app.use("/buyers", buyerAccountSettingsRouter)
 
