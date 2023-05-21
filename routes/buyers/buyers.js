b811e602e75280e4ad8e3b29e94723ed070ad26c
Ziqi Wang <ziqiwang@Ziqis-MacBook-Air.local>
2023-05-21 00:00:26 -0500

    create buyer sign up&in page

--- routes/buyers/buyers.js
+++ routes/buyers/buyers.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/signup", (req, res) => {
+    res.render("buyers/account/signup")
+})
+
+router.get("/signin", (req, res) => {
+    res.render("buyers/account/signin")
+})
+
 router.get("/:id", (req, res) => {
-    res.render("buyers/accountSettings")
+    res.render("buyers/account/accountSettings")
 })
 
 module.exports = router
