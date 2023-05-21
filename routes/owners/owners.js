6a09586ea7367c256fe8bc08c6d87991eeb9393c
Ziqi Wang <ziqiwang@Ziqis-MacBook-Air.local>
2023-05-21 00:05:14 -0500

    create owner sign up&in page

--- routes/owners/owners.js
+++ routes/owners/owners.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/signup", (req, res) => {
+    res.render("owners/account/signup")
+})
+
+router.get("/signin", (req, res) => {
+    res.render("owners/account/signin")
+})
+
 router.get("/:id", (req, res) => {
-    res.render("owners/accountSettings")
+    res.render("owners/account/accountSettings")
 })
 
 module.exports = router
