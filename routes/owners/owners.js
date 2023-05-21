b93eb95ecf65440715fdec4c781ce9f63f4cef74
Ziqi Wang <ziqiwang@Ziqis-MacBook-Air.local>
2023-05-21 00:28:02 -0500

    redirect owner to sign in page after submit sign up

--- routes/owners/owners.js
+++ routes/owners/owners.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/signup", (req, res) => {
-    res.render("owners/account/signup")
-})
+router
+    .route("/signup")
+    .get((req, res) => {
+        res.render("owners/account/signup")
+    })
+    .post((req, res) => {
+        res.redirect("http://localhost:3000/owners/signin")
+    })
 
 router.get("/signin", (req, res) => {
     res.render("owners/account/signin")
